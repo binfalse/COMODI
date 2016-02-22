0409cbe97f1983ba68d2d86b6025ac95d8bd40e3
Martin Scharm <martin@binfalse.de>
2016-02-22 13:11:26 +0100

    implemented some js to scroll to the correct location
    that's been a problem, because the overview figure
    is not loaded initially. when overview figure
    finished to load the browser displays the wrong
    location.. that's now fixed with jquery.

---
+++ web/template/js.js
@@ -0,0 +1,45 @@
+function reloadPostition ()
+{
+    var url = window.location.href;
+    $('html, body').animate(
+            {
+                scrollTop: $(url.substring (url.indexOf ("#"))).offset().top
+            }, 800);
+}
+
+function activateSmoothScroll ()
+{
+    if (!$("#objectproperties").length)
+    {
+        console.log ("page doesn't seem to be ready, yet. going to wait another 200ms");
+        setTimeout (activateSmoothScroll, 200);
+        return;
+    }
+    $('a[href*="#"]').click(function()
+            {
+                if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname)
+                {
+                    var target = $(this.hash);
+                    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                    if (target.length)
+                    {
+                        console.log ("tmp");
+                        $('html, body').animate(
+                                {
+                                    scrollTop: target.offset().top
+                                }, 1000);
+                        return false;
+                    }
+                }
+            });
+    if (window.location.href.indexOf ("#") > -1)
+        $("#overview-fig").load("image/svg+xml", function()
+                {
+                    reloadPostition ();
+                });
+}
+
+
+$(function() {
+    activateSmoothScroll ();
+});
